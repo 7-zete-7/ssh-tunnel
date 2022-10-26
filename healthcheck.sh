02415c8949448aae5c4b3d0ad6036341f2e572e1
Stanislau Kviatkouski <7zete7@gmail.com>
2022-10-26 12:46:52 +0300

    🐛 Fix incorrect case statement usage on healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -9,15 +9,15 @@ if [ -z ${LOCAL_PORT+x} ]; then
 	LOCAL_PORT=${REMOTE_SERVER_PORT}
 fi
 
-case "${HEALTHCHECK:-'soft'}" in
-	'none')
+case "${HEALTHCHECK:-soft}" in
+	none)
 		exit 0
 		;;
-	'soft')
+	soft)
 		netstat -ltn | grep -q "${LOCAL_PORT}"
 		exit $?
 		;;
-	'hard')
+	hard)
 		nc -w 0 -n -z 127.0.0.1 "${LOCAL_PORT}" >/dev/null 2>&1
 		exit $?
 		;;
